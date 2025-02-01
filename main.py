5bd4fd400b823b8d9e56dc082522b0da132ddcae
amr yasser loksha <founder@amrloksha151.top>
2025-02-01 20:56:25 +0200

    Creating window

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import tkinter as tk
 import ttkbootstrap as ttk
 
 def main():
-    window = ...
+    window = ttk.Window(title="Socket Mapper", size=[800, 600], themename="superhero")
+    # Scalability | Set it false for now and change it later during optimization
+    window.resizable(False, False)
+
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
