b6d70ac20866744260329307dc6270b3cf971c95
amr yasser loksha <founder@amrloksha151.top>
2025-02-02 14:29:37 +0200

    Image and buttons in the starting window

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ def main():
     title = ttk.Label(master=window, text="Socket Mapper", font="Jokerman 24 bold")
     title.pack()
 
+    # Central image only in the main page
+    image = ttk.Image.open("cable.png")
+    image = image.resize((400, 400))
+    tkImage = ttk.ImageTk.PhotoImage(image)
+    centralImage = ttk.Label(master=window, image=tkImage)
+    centralImage.pack()
+
+    # The following buttons should navigate to two different pages
+    btnFrame = ttk.Frame(master=window)
+    btnPortScanning = ttk.Button(master=btnFrame, text="Port Scanning")
+    btnBannerGrapping = ttk.Button(master=btnFrame, text="Banner Grapping")
+    btnFrame.pack(pady=20)
+    btnBannerGrapping.pack(side="left", padx=40)
+    btnPortScanning.pack(side="left", padx=40)
+
     window.mainloop()
 
 if __name__ == '__main__':
