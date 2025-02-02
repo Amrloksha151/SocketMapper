5f2fee887e1e18cca6765851492051d87341243f
amr yasser loksha <founder@amrloksha151.top>
2025-02-02 14:08:26 +0200

    Adding title and icon

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import tkinter as tk
 import ttkbootstrap as ttk
 
 def main():
-    window = ttk.Window(title="Socket Mapper", size=[800, 600], themename="superhero")
+    window = ttk.Window(title="Socket Mapper", size=[800, 600], themename="superhero", iconphoto="cable.png")
     # Scalability | Set it false for now and change it later during optimization
     window.resizable(False, False)
+
+    # The title of the running page
+    title = ttk.Label(master=window, text="Socket Mapper", font="Jokerman 24 bold")
+    title.pack()
 
     window.mainloop()
 
