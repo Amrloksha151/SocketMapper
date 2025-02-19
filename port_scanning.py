00cc8815f93c00ad25d557035f4a727b9ad8882c
amr yasser loksha <founder@amrloksha151.top>
2025-02-19 10:28:21 +0200

    adding save method

--- port_scanning.py
+++ port_scanning.py
@@ -1,13 +1,17 @@
 import socket
+import csv
 
 class Scanning:
-    def __init__(self, protocol, host, start, stop):
+    def __init__(self, protocol, host, start, stop, txt, csv):
         self.protocol = protocol
         self.host = host
         self.start = start
         self.stop = stop
+        self.txt = txt
+        self.csv = csv
     
     def scan(self):
+        self.results = []
         if self.protocol == 'TCP':
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
@@ -16,13 +20,16 @@ class Scanning:
             try:
                 client.connect((self.host, port))
             except Exception:
-                yield 'Closed'
+                self.results.append('Closed')
             else:
-                yield 'Open'
+                self.results.append('Opened')
+    
+    def save(self):
+        ...
     
     def multithreadedScan(self):
         ...
     
     def __str__(self):
         return 'Port Scanning --> use scan method for normal scanning and multithreaded for faster scanning'
-        
+    
