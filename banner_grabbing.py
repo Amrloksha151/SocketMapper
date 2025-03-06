8b827ff61300b99c1b9e7ae66f0b0eec98638842
amr yasser loksha <founder@amrloksha151.top>
2025-03-06 16:25:48 +0200

    editing request

--- banner_grabbing.py
+++ banner_grabbing.py
@@ -1,3 +1,4 @@
 import socket
 
-request = "GET / HTTP/1.1\r\nHost: target.com\r\nUser-Agent: Mozilla/5.0\r\n\r\n"
+target = ...
+request = "GET / HTTP/1.1\r\nHost: %s\r\nUser-Agent: Mozilla/5.0\r\n\r\n" % target
